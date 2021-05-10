2bbdc0f55fe9d18bdda6c9a77a0ad1ad34ae3d57
Anna Salmane <77804835+AnnaSalmane@users.noreply.github.com>
2021-05-10 08:31:04 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,293 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Personal page</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,300;0,500;0,700;1,300;1,500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+</head>
+
+<body>
+    <div class="container">
+        <nav class="navbar">
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#profile">Profile</a></li>
+                <li><a href="#experiences">Experiences</a></li>
+                <li><a href="#skills">Skills</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+        <section id="home">
+            <div id="home__glass">
+                <h1>Anna Salmane</h1>
+                <hr>
+                <h2>My personal page</h2>
+            </div>
+        </section>
+        <section id="profile">
+            <h1>Profile</h1>
+            <div class="profile_container">
+                <div id="my_picture">
+                    <img src="woman.jpg" alt="My picture">
+                </div>
+                <div id="about_me">
+                    <h2>About me</h2>
+                    <p>llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll</p>
+                </div>
+            </div>
+        </section>
+        <section id="experiences">
+            <h1>Experiences</h1>
+            <div id="educations">
+                <h2>Educations</h2>
+                <h3>Riga Technical university</h3>
+                <h4>Civil engeneering</h4>
+                <p>sfjhkzfhkxdfkxrhfgxdhlvkglfxdvlxdrjghkxdjlfvkxfuhxgkxdrgkxdhhrgjhxdkf</p>
+            </div>
+            <div id="careers">
+                <h2>Careers</h2>
+                <h3>8CBR</h3>
+                <h4>Project assistant</h4>
+                <p>fzhodufgvzsohfouzvsdhffffzsvoufhozuefvkzhsegolzughfkzhsjkzhsujhfkh</p>
+            </div>
+        </section>
+        <section id="skills">
+            <h1>Skills</h1>
+            <ul>
+                <li>
+                    <p>HTML</p>
+                    <span class="bar"><span class="html high"></span></span>
+                </li>
+                <li>
+                    <p>CSS</p>
+                    <span class="bar"><span class="css mid"></span></span>
+                </li>
+                <li>
+                    <p>SASS</p>
+                    <span class="bar"><span class="sass mid"></span></span>
+                </li>
+                <li>
+                    <p>PHP</p>
+                    <span class="bar"><span class="php lov"></span></span>
+                </li>
+                <li>
+                    <p>JAVASCRIPT</p><span class="bar"><span class="javascript lov"></span></span>
+                </li>
+                <li>
+                    <p>JQUERY</p><span class="bar"><span class="jquery lov"></span></span>
+                </li>
+                <li>
+                    <p>VUE</p><span class="bar"><span class="vue lov"></span></span>
+                </li>
+                <li>
+                    <p>REACT</p><span class="bar"><span class="react lov"></span></span>
+                </li>
+                <li>
+                    <p>GIT</p><span class="bar"><span class="git mid"></span></span>
+                </li>
+                <li>
+                    <p>BOOTSTRAP</p><span class="bar"><span class="bootstrap mid"></span></span>
+                </li>
+                <li>
+                    <p>FIGMA</p><span class="bar"><span class="figma mid"></span></span>
+                </li>
+            </ul>
+        </section>
+        <section id="projects">
+            <h1>Projects</h1>
+            <div class="row">
+                <div class="projects_col">
+                    <a href="https://github.com/AnnaSalmane/PHP_examples_bootcamp" target="_blank">
+                        <img src="/bootcamp_final_project/images/php_tictactoe.png" alt="project_1">
+                        <div class="layer">
+                            <p>
+                                In this project use: <br>
+                                -HTML; <br>
+                                -CSS; <br>
+                                -PHP
+                            </p>
+                        </div>
+                    </a>
+                </div>
+                <div class="projects_col">
+                    <a href="https://github.com/AnnaSalmane/Database_Bootcamp" target="_blank">
+                        <img src="/bootcamp_final_project/images/database.png" alt="project_1">
+                        <div class="layer">
+                            <p>
+                                In this project use: <br>
+                                -HTML; <br>
+                                -CSS; <br>
+                                -PHP; <br>
+                                -MySQL
+                            </p>
+                        </div>
+                    </a>
+                </div>
+                <div class="projects_col">
+                    <a href="https://github.com/AnnaSalmane/PHP_examples_bootcamp" target="_blank">
+                        <img src="/bootcamp_final_project/images/42_link.png" alt="project_1">
+                        <div class="layer">
+                            <p>
+                                In this project use: <br>
+                                -HTML; <br>
+                                -CSS; <br>
+                                -PHP; <br>
+                                -Bootstrap
+
+                            </p>
+                        </div>
+                    </a>
+                </div>
+                <div class="projects_col">
+                    <a href="https://github.com/AnnaSalmane/all_components" target="_blank">
+                        <img src="/bootcamp_final_project/images/all_components.png" alt="project_1">
+                        <div class="layer">
+                            <p>
+                                In this project use: <br>
+                                -HTML; <br>
+                                -SASS; <br>
+                                -PHP; <br>
+                                -VUE.JS
+                            </p>
+                        </div>
+                    </a>
+                </div>
+                <div class="projects_col">
+                    <a href="https://github.com/AnnaSalmane/Light-dark-mode" target="_blank">
+                        <img src="/bootcamp_final_project/images/light_dark_mode.png" alt="project_1">
+                        <div class="layer">
+                            <p>
+                                In this project use: <br>
+                                -HTML; <br>
+                                -CSS; <br>
+                                -JS
+                            </p>
+                        </div>
+                    </a>
+                </div>
+                <div class="projects_col">
+                    <a href="https://github.com/AnnaSalmane/Robo_Friends" target="_blank">
+                        <img src="/bootcamp_final_project/images/robo_friends.png" alt="project_1">
+                        <div class="layer">
+                            <p>
+                                In this project use: <br>
+                                -HTML; <br>
+                                -CSS; <br>
+                                -REACT
+                            </p>
+                        </div>
+                    </a>
+                </div>
+
+            </div>
+        </section>
+
+        <?php
+
+        echo "<pre>";
+        print_r($_POST);
+        echo "</pre>";
+
+        $message_sent = false;
+        if (isset($_POST['email']) && $_POST['email'] != '') {
+            if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+                $firstName = $_POST['firstName'];
+                $lastName = $_POST['lastName'];
+                $userEmail = $_POST['email'];
+                $meetingDate = $_POST['date'];
+                $meetingTime = $_POST['time'];
+
+                $to = $userEmail;
+                $body = "Dear " . $firstName . ", your meeting time at " . $meetingDate . " " . $meetingTime  . " is reserve. I will contact with You soon!";
+
+                $body .= "From: " . $firstName . " " . $lastName . "\r\n";
+                $body .= "Email: " . $userEmail . "\r\n";
+                $body .= "Meeting date and time: " . $meetingDate . " " . $meetingTime . "\r\n";
+
+                mail($to, $firstName, $body);
+
+                $message_sent = true;
+            }
+        }
+        ?>
+        <section id="contact">
+            <div class="contact_container">
+                <div class="contactinfo">
+                    <div>
+                        <h2>Contact info</h2>
+                        <ul class="info">
+                            <li>
+                                <span><i class="fas fa-globe-europe"></i></span>
+                                <span>Sigulda, Latvia</span>
+                            </li>
+                            <li>
+                                <span><i class="fas fa-envelope"></i></span>
+                                <span>annasalmane111@gmail.com</span>
+                            </li>
+                            <li>
+                                <span><i class="fas fa-phone-alt"></i></span>
+                                <span>+371 25941058</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <ul class="sci">
+                        <li><a href="https://github.com/AnnaSalmane" target="_blank"><i class="fab fa-github"></i></a></li>
+                        <li><a href="https://www.linkedin.com/feed/" target="_blank"><i class="fab fa-linkedin-in"></i></a></li>
+                        <li><a href="https://www.facebook.com/" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+                        <li><a href="https://www.instagram.com/annas_sa/" target="_blank"><i class="fab fa-instagram"></i></a></li>
+                    </ul>
+                </div>
+                <form action="" method="POST">
+                    <div class="contactForm">
+                        <?php
+                        if ($message_sent) :
+                        ?>
+                            <h2>
+                                Thanks for meeting time reservation!
+                            </h2>
+                        <?php
+                        else :
+                        ?>
+                            <h2>Make a meeting reservation</h2>
+                            <div class="formBox">
+                                <div class="inputBox w50">
+                                    <input type="text" name="firstName" required>
+                                    <span>First Name</span>
+                                </div>
+                                <div class="inputBox w50">
+                                    <input type="text" name="lastName" required>
+                                    <span>Last Name</span>
+                                </div>
+                                <div class="inputBox w50">
+                                    <input type="email" name="email" required>
+                                    <span>Email adress</span>
+                                </div>
+                                <div class="inputBox w50">
+                                    <input type="text" name="mobileNr" required>
+                                    <span>Mobile number</span>
+                                </div>
+                                <div class="inputBox w50">
+                                    <input type="date" name="date" required>
+                                </div>
+                                <div class="inputBox w50">
+                                    <input type="time" name="time" required>
+                                </div>
+                                <div class="inputBox w100">
+                                    <input type="submit" value="Send" id="submit">
+                                </div>
+
+                            </div>
+                        <?php
+                        endif;
+                        ?>
+                    </div>
+                </form>
+            </div>
+    </div>
+    </section>
+    </div>
+</body>
